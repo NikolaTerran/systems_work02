573cd97c91e150926f52ca4f9cebebc7ce521d08
NikolaTerran <nikolaterranthe1@gmail.com>
2018-09-16 20:53:01 -0400

    Add files via upload

---
+++ hw02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	unsigned int a =  4000000000;
+	char *b = &a;
+	printf("4000000000 in hexadecimal.\n");
+	printf("%x\n",a);
+	
+	*b += 1;
+        printf("increment the reference value by 1\n");
+	printf("%x\n",a);
+	
+	printf("increment the reference by 1, then
+			increase the referenced value by 16.\n");
+	b++;
+	*b += 16;
+
+	printf("%x\n",a);
+	printf("voila!");
+
+	return 0;
+
+	
+}
